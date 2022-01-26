fb42c4a909657f6e6e5bc02d95b7555fcc9a051b
sigemype <sigemype@gmail.com>
2022-01-26 20:34:46 +0000

    Cambios en api, App

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,41 @@ if ($hostname) {
 
         Route::post('login', 'Tenant\Api\MobileController@login');
 
+        //reportes caja
+        Route::get('cash/report/products/{cash}', 'Tenant\Api\MobileController@report_products');
+        Route::get('cash/report/report-ticket/{cash}', 'Tenant\Api\MobileController@reportTicket');
+        Route::get('cash/report/report-a4/{cash}', 'Tenant\Api\MobileController@reportA4');
+        Route::get('cash/report/income-summary/{cash}', 'Tenant\Api\MobileController@pdf');
+        
         Route::middleware(['auth:api', 'locked.tenant'])->group(function () {
-            //MOBILE
+
+            // caja
+        Route::get('cash/open/{value}', 'Tenant\Api\MobileController@opencash');
+        Route::get('cash/check', 'Tenant\Api\MobileController@opening_cash_check');
+        Route::get('cash/records', 'Tenant\Api\MobileController@records');
+        Route::get('cash/email', 'Tenant\Api\MobileController@cashemail');
+        Route::get('cash/close/{cash}', 'Tenant\Api\MobileController@close');
+
+        
+        Route::post('cash/report/email', 'Tenant\Api\MobileController@email');
+
+
+
+
+
+            //MOBILE 
             Route::get('document/series', 'Tenant\Api\MobileController@getSeries');
+            Route::get('document/documents_count', 'Tenant\Api\MobileController@documents_count');
             Route::get('document/paymentmethod', 'Tenant\Api\MobileController@getPaymentmethod');
             Route::get('document/tables', 'Tenant\Api\MobileController@tables');
             Route::get('document/customers', 'Tenant\Api\MobileController@customers');
+            Route::get('document/customers/{id}', 'Tenant\Api\MobileController@customers_details');
             Route::post('document/email', 'Tenant\Api\MobileController@document_email');
             Route::post('sale-note', 'Tenant\Api\SaleNoteController@store');
             Route::get('sale-note/series', 'Tenant\Api\SaleNoteController@series');
             Route::get('sale-note/lists', 'Tenant\Api\SaleNoteController@lists');
             Route::post('item', 'Tenant\Api\MobileController@item');
+            Route::get('items/details/{id}', 'Tenant\Api\MobileController@item_details');
             Route::post('items/{id}/update', 'Tenant\Api\MobileController@updateItem');
             Route::post('item/upload', 'Tenant\Api\MobileController@upload');
             Route::post('person', 'Tenant\Api\MobileController@person');
@@ -25,7 +49,6 @@ if ($hostname) {
             Route::post('sale-note/email', 'Tenant\Api\SaleNoteController@email');
             Route::post('sale-note/{id}/generate-cpe', 'Tenant\Api\SaleNoteController@generateCPE');
 
-            Route::get('report', 'Tenant\Api\MobileController@report');
             Route::get('report/{year}/{month}/{day}', 'Tenant\Api\MobileController@report');
 
             Route::post('documents', 'Tenant\Api\DocumentController@store');
@@ -47,10 +70,6 @@ if ($hostname) {
 
             Route::post('documents_server', 'Tenant\Api\DocumentController@storeServer');
             Route::get('document_check_server/{external_id}', 'Tenant\Api\DocumentController@documentCheckServer');
-
-            //liquidacion de compra
-            Route::post('purchase-settlements', 'Tenant\Api\PurchaseSettlementController@store');
-
         });
         Route::get('documents/search/customers', 'Tenant\DocumentController@searchCustomers');
 
@@ -66,7 +85,6 @@ if ($hostname) {
         //reseller
         Route::post('reseller/detail', 'System\Api\ResellerController@resellerDetail');
         Route::post('reseller/lockedAdmin', 'System\Api\ResellerController@lockedAdmin');
-        Route::post('reseller/lockedTenant', 'System\Api\ResellerController@lockedTenant');
 
     });
 
