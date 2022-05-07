b7a721446e3c123bc823ec9f4b1e48a30718f3f3
jxux <jxux25@gmail.com>
2022-05-07 09:10:43 -0500

    Correccion en sumary.xml

--- app/CoreFacturalo/Templates/xml/summary.blade.php
+++ app/CoreFacturalo/Templates/xml/summary.blade.php
@@ -1,4 +1,4 @@
-{!! '<?xml version="1.0" encoding="utf-8" standalone="no"?>' !!}
+{!!  '<'.'?xml version="1.0" encoding="utf-8" standalone="no"?'.'>'  !!}
 <SummaryDocuments
         xmlns="urn:sunat:names:specification:ubl:peru:schema:xsd:SummaryDocuments-1"
         xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
