5079cf818fe6ba26319402f98add8f6b754000a3
jxux <35107645+jxux@users.noreply.github.com>
2023-02-13 07:19:47 -0500

    Cambios prod

--- modules/Pos/Http/Controllers/CashController.php
+++ modules/Pos/Http/Controllers/CashController.php
@@ -108,6 +108,7 @@ class CashController extends Controller
         $cash_final_balance = 0;
         $cash_documents = $cash->cash_documents;
         $all_documents = [];
+        $pays = [];
 
         // Metodos de pago de no credito
         $methods_payment_credit = PaymentMethodType::NonCredit()->get()->transform(function ($row) {
@@ -219,12 +220,11 @@ class CashController extends Controller
                     'type_transaction'          => 'Venta',
                     'document_type_description' => 'NOTA DE VENTA',
                     'number'                    => $sale_note->number_full,
-                    'date_of_issue'             => $date_payment,
+                    'date_of_issue'             => $date_payment ? $date_payment->format('Y-m-d') : null ,
                     'date_sort'                 => $sale_note->date_of_issue,
                     'customer_name'             => $sale_note->customer->name,
                     'customer_number'           => $sale_note->customer->number,
-                    'total'                     => ((!in_array($sale_note->state_type_id, $status_type_id)) ? 0
-                        : $sale_note->total),
+                    'total'                     => ((!in_array($sale_note->state_type_id, $status_type_id)) ? 0 : $sale_note->total),
                     'currency_type_id'          => $sale_note->currency_type_id,
                     'usado'                     => $usado." ".__LINE__,
                     'tipo'                      => 'sale_note',
@@ -367,7 +367,7 @@ class CashController extends Controller
                 // fin items
             }
             /** Documentos de Tipo Servicio tecnico */
-            elseif ($cash_document->technical_service) 
+            elseif ($cash_document->technical_service)
             {
                 $usado = '<br>Se usan para cash<br>';
                 $technical_service = $cash_document->technical_service;
