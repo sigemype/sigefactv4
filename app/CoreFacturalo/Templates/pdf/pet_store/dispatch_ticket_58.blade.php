5d8d3a59b0a7726be9fe4b458eec49f6189a7ff5
Sigemype <jxux25@gmail.com>
2023-04-11 14:30:49 -0500

    dispatch pet_store pdf

--- app/CoreFacturalo/Templates/pdf/pet_store/dispatch_ticket_58.blade.php
+++ app/CoreFacturalo/Templates/pdf/pet_store/dispatch_ticket_58.blade.php
@@ -4,7 +4,11 @@
     //$path_style = app_path('CoreFacturalo'.DIRECTORY_SEPARATOR.'Templates'.DIRECTORY_SEPARATOR.'pdf'.DIRECTORY_SEPARATOR.'style.css');
 
     $document_number = $document->series.'-'.str_pad($document->number, 8, '0', STR_PAD_LEFT);
-    // $document_type_driver = App\Models\Tenant\Catalogs\IdentityDocumentType::findOrFail($document->driver->identity_document_type_id);
+
+    $logo = "storage/uploads/logos/{$company->logo}";
+    if($establishment->logo) {
+        $logo = "{$establishment->logo}";
+    }
 
 @endphp
 <html>
@@ -13,39 +17,45 @@
     {{--<link href="{{ $path_style }}" rel="stylesheet" />--}}
 </head>
 <body>
+@if($company->logo)
+    <div class="text-center company_logo_box pt-5">
+        <img src="data:{{mime_content_type(public_path("{$logo}"))}};base64, {{base64_encode(file_get_contents(public_path("{$logo}")))}}" alt="{{$company->name}}" class="company_logo_ticket contain">
+    </div>
+{{--@else--}}
+    {{--<div class="text-center company_logo_box pt-5">--}}
+        {{--<img src="{{ asset('logo/logo.jpg') }}" class="company_logo_ticket contain">--}}
+    {{--</div>--}}
+@endif
+
 <table class="full-width">
     <tr>
-        @if($company->logo)
-            <td width="10%">
-                <img
-                    src="data:{{mime_content_type(public_path("storage/uploads/logos/{$company->logo}"))}};base64, {{base64_encode(file_get_contents(public_path("storage/uploads/logos/{$company->logo}")))}}"
-                    alt="{{$company->name}}" alt="{{ $company->name }}" class="company_logo" style="max-width: 300px">
-            </td>
-        @else
-            <td width="10%">
-                {{--<img src="{{ asset('logo/logo.jpg') }}" class="company_logo" style="max-width: 150px">--}}
-            </td>
-        @endif
-        <td width="50%" class="pl-3">
-            <div class="text-left">
-                <h3 class="">{{ $company->name }}</h3>
-                <h4>{{ 'RUC '.$company->number }}</h4>
-                <h5 style="text-transform: uppercase;">
-                    {{ ($establishment->address !== '-')? $establishment->address : '' }}
-                    {{ ($establishment->district_id !== '-')? ', '.$establishment->district->description : '' }}
-                    {{ ($establishment->province_id !== '-')? ', '.$establishment->province->description : '' }}
-                    {{ ($establishment->department_id !== '-')? '- '.$establishment->department->description : '' }}
-                </h5>
-                <h5>{{ ($establishment->email !== '-')? $establishment->email : '' }}</h5>
-                <h5>{{ ($establishment->telephone !== '-')? $establishment->telephone : '' }}</h5>
-            </div>
-        </td>
-        <td width="40%" class="border-box p-4 text-center">
-            <h4 class="text-center">{{ $document->document_type->description }}</h4>
-            <h3 class="text-center">{{ $document_number }}</h3>
+        <td class="text-center"><h4>{{ $company->name }}</h4></td>
+    </tr>
+    <tr>
+        <td class="text-center"><h5>{{ 'RUC '.$company->number }}</h5></td>
+    </tr>
+    <tr>
+        <td class="text-center" style="text-transform: uppercase;">
+            {{ ($establishment->address !== '-')? $establishment->address : '' }}
+            {{ ($establishment->district_id !== '-')? ', '.$establishment->district->description : '' }}
+            {{ ($establishment->province_id !== '-')? ', '.$establishment->province->description : '' }}
+            {{ ($establishment->department_id !== '-')? '- '.$establishment->department->description : '' }}
         </td>
     </tr>
+    <tr>
+        <td class="text-center">{{ ($establishment->email !== '-')? $establishment->email : '' }}</td>
+    </tr>
+    <tr>
+        <td class="text-center pb-3">{{ ($establishment->telephone !== '-')? $establishment->telephone : '' }}</td>
+    </tr>
+    <tr>
+        <td class="text-center pt-3 border-top"><h4>{{ $document->document_type->description }}</h4></td>
+    </tr>
+    <tr>
+        <td class="text-center pb-3 border-bottom"><h3>{{ $document_number }}</h3></td>
+    </tr>
 </table>
+
 <table class="full-width border-box mt-10 mb-10">
     <thead>
     <tr>
@@ -57,25 +67,20 @@
         <td>Razón Social: {{ $customer->name }}</td>
     </tr>
     <tr>
-        <td>{{ $customer->identity_document_type->description }}: {{ $customer->number }}
+        <td>RUC: {{ $customer->number }}
         </td>
     </tr>
     <tr>
-        @if($document->transfer_reason_type_id === '09')
-            <td>Dirección: {{ $customer->address }} - {{ $customer->country->description }}
-            </td>
-        @else
-            <td>Dirección: {{ $customer->address }}
-                {{ ($customer->district_id !== '-')? ', '.$customer->district->description : '' }}
-                {{ ($customer->province_id !== '-')? ', '.$customer->province->description : '' }}
-                {{ ($customer->department_id !== '-')? '- '.$customer->department->description : '' }}
-            </td>
-        @endif
+        <td>Dirección: {{ $customer->address }}
+            {{ ($customer->district_id !== '-')? ', '.$customer->district->description : '' }}
+            {{ ($customer->province_id !== '-')? ', '.$customer->province->description : '' }}
+            {{ ($customer->department_id !== '-')? '- '.$customer->department->description : '' }}
+        </td>
     </tr>
     @if ($customer->telephone)
-        <tr>
-            <td>Teléfono:{{ $customer->telephone }}</td>
-        </tr>
+    <tr>
+        <td>Teléfono:{{ $customer->telephone }}</td>
+    </tr>
     @endif
     <tr>
         <td>Vendedor: {{ $document->user->name }}</td>
@@ -98,37 +103,23 @@
         <td>Modalidad de Transporte: {{ $document->transport_mode_type->description }}</td>
     </tr>
 
-    @if($document->transfer_reason_description)
-        <tr>
-            <td colspan="2">Descripción de motivo de traslado: {{ $document->transfer_reason_description }}</td>
-        </tr>
-    @endif
-
     @if($document->related)
-        <tr>
-            <td>Número de documento (DAM): {{ $document->related->number }}</td>
-            <td>Tipo documento relacionado: {{ $document->getRelatedDocumentTypeDescription() }}</td>
-        </tr>
+    <tr>
+        <td>Número de documento (DAM): {{ $document->related->number }}</td>
+        <td>Tipo documento relacionado: {{ $document->getRelatedDocumentTypeDescription() }}</td>
+    </tr>
     @endif
 
     <tr>
         <td>Peso Bruto Total({{ $document->unit_type_id }}): {{ $document->total_weight }}</td>
         @if($document->packages_number)
-            <td>Número de Bultos: {{ $document->packages_number }}</td>
+        <td>Número de Bultos: {{ $document->packages_number }}</td>
         @endif
     </tr>
     <tr>
-        <td colspan="2">P.Partida: {{ $document->origin->location_id }} - {{ $document->origin->address }}</td>
+        <td>P.Partida: {{ $document->origin->location_id }} - {{ $document->origin->address }}</td>
+        <td>P.Llegada: {{ $document->delivery->location_id }} - {{ $document->delivery->address }}</td>
     </tr>
-    <tr>
-        <td colspan="2">P.Llegada: {{ $document->delivery->location_id }} - {{ $document->delivery->address }}</td>
-    </tr>
-    @if($document->order_form_external)
-        <tr>
-            <td>Orden de pedido: {{ $document->order_form_external }}</td>
-            <td></td>
-        </tr>
-    @endif
     </tbody>
 </table>
 <table class="full-width border-box mt-10 mb-10">
@@ -142,17 +133,17 @@
         @php
             $document_type_dispatcher = App\Models\Tenant\Catalogs\IdentityDocumentType::findOrFail($document->dispatcher->identity_document_type_id);
         @endphp
-        <tr>
-            <td>Nombre y/o razón social: {{ $document->dispatcher->name }}</td>
-            <td>{{ $document_type_dispatcher->description }}: {{ $document->dispatcher->number }}</td>
-        </tr>
+    <tr>
+        <td>Nombre y/o razón social: {{ $document->dispatcher->name }}</td>
+        <td>{{ $document_type_dispatcher->description }}: {{ $document->dispatcher->number }}</td>
+    </tr>
     @else
     <tr>
         @if($document->transport_data)
-            <td>Número de placa del vehículo: {{ $document->transport_data['plate_number'] }}</td>
+        <td>Número de placa del vehículo: {{ $document->transport_data['plate_number'] }}</td>
         @endif
         @if($document->driver->number)
-            <td>Conductor: {{ $document->driver->number }}</td>
+        <td>Conductor: {{ $document->driver->number }}</td>
         @endif
     </tr>
     <tr>
@@ -231,27 +222,27 @@
 </table>
 
 @if($document->observations)
-    <table class="full-width border-box mt-10 mb-10">
-        <tr>
-            <td class="text-bold border-bottom font-bold">OBSERVACIONES</td>
-        </tr>
-        <tr>
-            <td>{{ $document->observations }}</td>
-        </tr>
-    </table>
+<table class="full-width border-box mt-10 mb-10">
+    <tr>
+        <td class="text-bold border-bottom font-bold">OBSERVACIONES</td>
+    </tr>
+    <tr>
+        <td>{{ $document->observations }}</td>
+    </tr>
+</table>
 @endif
 
 @if ($document->reference_document)
-    <table class="full-width border-box">
-        @if($document->reference_document)
-            <tr>
-                <td class="text-bold border-bottom font-bold">{{$document->reference_document->document_type->description}}</td>
-            </tr>
-            <tr>
-                <td>{{ ($document->reference_document) ? $document->reference_document->number_full : "" }}</td>
-            </tr>
-        @endif
-    </table>
+<table class="full-width border-box">
+    @if($document->reference_document)
+    <tr>
+        <td class="text-bold border-bottom font-bold">{{$document->reference_document->document_type->description}}</td>
+    </tr>
+    <tr>
+        <td>{{ ($document->reference_document) ? $document->reference_document->number_full : "" }}</td>
+    </tr>
+    @endif
+</table>
 @endif
 @if ($document->data_affected_document)
     @php
@@ -279,62 +270,64 @@
     @endif
 @endif
 @if ($document->reference_order_form_id)
-    <table class="full-width border-box">
-        @if($document->order_form)
-            <tr>
-                <td class="text-bold border-bottom font-bold">ORDEN DE PEDIDO</td>
-            </tr>
-            <tr>
-                <td>{{ ($document->order_form) ? $document->order_form->number_full : "" }}</td>
-            </tr>
-        @endif
-    </table>
+<table class="full-width border-box">
+    @if($document->order_form)
+    <tr>
+        <td class="text-bold border-bottom font-bold">ORDEN DE PEDIDO</td>
+    </tr>
+    <tr>
+        <td>{{ ($document->order_form) ? $document->order_form->number_full : "" }}</td>
+    </tr>
+    @endif
+</table>
 
 @elseif ($document->order_form_external)
-    <table class="full-width border-box">
-        <tr>
-            <td class="text-bold border-bottom font-bold">ORDEN DE PEDIDO</td>
-        </tr>
-        <tr>
-            <td>{{ $document->order_form_external }}</td>
-        </tr>
-    </table>
-
-@endif
-
-
-@if ($document->reference_sale_note_id)
-    <table class="full-width border-box">
-        @if($document->sale_note)
-            <tr>
-                <td class="text-bold border-bottom font-bold">NOTA DE VENTA</td>
-            </tr>
-            <tr>
-                <td>{{ ($document->sale_note) ? $document->sale_note->number_full : "" }}</td>
-            </tr>
-        @endif
-    </table>
-@endif
-@if($document->qr)
-<table class="full-width">
+<table class="full-width border-box">
     <tr>
-        <td class="text-left">
-            <img src="data:image/png;base64, {{ $document->qr }}" style="margin-right: -10px;"/>
-        </td>
+        <td class="text-bold border-bottom font-bold">ORDEN DE PEDIDO</td>
+    </tr>
+    <tr>
+        <td>{{ $document->order_form_external }}</td>
     </tr>
 </table>
+
 @endif
+
+@if ($document->reference_sale_note_id)
+<table class="full-width border-box">
+    @if($document->sale_note)
+    <tr>
+        <td class="text-bold border-bottom font-bold">NOTA DE VENTA</td>
+    </tr>
+    <tr>
+        <td>{{ ($document->sale_note) ? $document->sale_note->number_full : "" }}</td>
+    </tr>
+    @endif
+</table>
+@endif
+
+@if($document->qr)
+    <table class="full-width">
+        <tr>
+            <td class="text-left">
+                <img src="data:image/png;base64, {{ $document->qr }}" style="margin-right: -10px;"/>
+            </td>
+        </tr>
+    </table>
+@endif
+
 @if ($document->terms_condition)
     <br>
     <table class="full-width">
         <tr>
             <td>
-                <h6 style="font-size: 12px; font-weight: bold;">Términos y condiciones del servicio</h6>
+                <h6 style="font-size: 10px; font-weight: bold;">Términos y condiciones del servicio</h6>
                 {!! $document->terms_condition !!}
             </td>
         </tr>
     </table>
 @endif
+<br>
 
 </body>
 </html>
